6516c6b89aa31f530d9ab004e3565e1588c20378
carlinne <565963044@qq.com>
2023-05-23 19:20:17 +0800

    fix: url拼接错误

--- src/utils/wallpaper.ts
+++ src/utils/wallpaper.ts
@@ -2,10 +2,10 @@ import { Category } from '../types/category';
 import { debounceRequest } from './axiosWrapper'
 
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7002/api' : 'https//carline.life/api';
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7002/api' : 'https://www.carline.life/api';
 
 export const getWallpaper = (category: Category, pageStart: number = 0, pageTotal: number = 10) => {
     // category pageStart pageTotal query 拼接到url上
-    const url = `${baseUrl}?category=${category}&pageStart=${pageStart}&pageTotal=${pageTotal}`;
+    const url = `${'https://www.carline.life/api'}?category=${category}&pageStart=${pageStart}&pageTotal=${pageTotal}`;
     return debounceRequest(url, 'GET', null, 300);
 }
